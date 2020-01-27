4ea5a44cf4d08f209d20038df39f206f1f3fc4b3
michalmalara <55634937+michalmalara@users.noreply.github.com>
2020-01-27 15:36:34 +0100

    resizing blocked, progress bar and  monitoring for user added

--- join_xls.py
+++ join_xls.py
@@ -10,11 +10,14 @@ class Ui(QtWidgets.QMainWindow):
 
         uic.loadUi('mainWindow.ui', self)
 
+        self.setFixedSize(self.size())
+
         self.btnPickSource = self.findChild(QtWidgets.QPushButton, 'btnPickSource')
         self.btnPickTarget = self.findChild(QtWidgets.QPushButton, 'btnPickTarget')
         self.btnStart = self.findChild(QtWidgets.QPushButton, 'btnStart')
         self.lblSourcePath = self.findChild(QtWidgets.QLabel, 'lblSourcePath')
         self.lblTargetPath = self.findChild(QtWidgets.QLabel, 'lblTargetPath')
+        self.lblCurrentFile = self.findChild(QtWidgets.QLabel, 'lblCurrentFile')
 
         self.btnPickSource.clicked.connect(self.pickFiles)
         self.btnPickTarget.clicked.connect(self.setTargetFile)
@@ -30,7 +33,7 @@ class Ui(QtWidgets.QMainWindow):
         files, _ = QtWidgets.QFileDialog.getOpenFileNames(self, "Wybierz pliki do scalenia...", "",
                                                           "Excel Files (*.xls)", options=options)
         if files:
-            self.lblSourcePath.setText(f'{files[0]} + {len(files)-1} plików')
+            self.lblSourcePath.setText(f'{files[0]} + {len(files) - 1} plików')
             self.files = files
 
     def setTargetFile(self):
@@ -47,14 +50,15 @@ class Ui(QtWidgets.QMainWindow):
         done = False
         try:
             if len(self.files) > 0 and len(self.target) > 0:
-                if len(self.files)>=2:
-                    done = executeJoin(self.files, self.target)
+                if len(self.files) >= 2:
+                    done = self.executeJoin(self.files, self.target)
                 else:
                     self.showPopup('Wybierz więcej, niż jeden plik', 'Błąd!', QMessageBox.Warning)
             else:
-                self.showPopup('Wybierz pliki do scalenia oraz podaj lokalizację i nazwę nowego pliku!', 'BŁĄD!!!', QMessageBox.Warning)
+                self.showPopup('Wybierz pliki do scalenia oraz podaj lokalizację i nazwę nowego pliku!', 'BŁĄD!!!',
+                               QMessageBox.Warning)
             if done:
-                self.showPopup(f'Scalono {len(self.files)+1} plików do pliku:\n{self.target}', 'Sukces!')
+                self.showPopup(f'Scalono {len(self.files) + 1} plików do pliku:\n{self.target}', 'Sukces!')
         except Exception as e:
             self.showPopup(f'Skontaktuj się z twórcą programu. Prześlij mu poniższy tekst:\n{e}', 'BŁĄD KRYTYCZNY!!!',
                            QMessageBox.Critical)
@@ -68,26 +72,27 @@ class Ui(QtWidgets.QMainWindow):
 
         x = msg.exec_()
 
-
-def executeJoin(filelist, targetFile):
-    try:
-        result = pd.read_excel(filelist.pop(), header=1)
-    except Exception as e:
-        print('Błąd: ', e)
-
-    for file in filelist:
+    def executeJoin(self, filelist, targetFile):
         try:
-            file_content = pd.read_excel(file, header=1)
-            result = pd.concat([result, file_content])
+            self.lblCurrentFile.setText(filelist[0])
+            result = pd.read_excel(filelist.pop(), header=1)
+
+        except Exception as e:
+            print('Błąd: ', e)
+        for file in filelist:
+            try:
+                file_content = pd.read_excel(file, header=1)
+                self.lblCurrentFile.setText(file)
+                result = pd.concat([result, file_content])
+            except Exception as e:
+                print('Błąd: ', e)
+
+        try:
+            result.to_excel(targetFile, index=False)
         except Exception as e:
             print('Błąd: ', e)
 
-    try:
-        result.to_excel(targetFile, index=False)
-    except Exception as e:
-        print('Błąd: ', e)
-
-    return True
+        return True
 
 
 if __name__ == '__main__':
